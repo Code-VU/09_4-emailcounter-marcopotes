e58240bf6c8a2ec279934ae64b98f3063ec26699
Marco Potes <potesmarco@yahoo.com>
2024-05-11 01:02:25 -0500

    First Attempt

--- counter.py
+++ counter.py
@@ -3,8 +3,33 @@ def countEmail():
     name = input("Enter file:")
     if len(name) < 1 : name = "mbox-short.txt"
     handle = open(name)
+
+    di = dict()
+
+    for lin in handle:
+
+        if lin.startswith("From "):
+            lin = lin.split()
+            sender = lin[1]
+            
+            if sender in di:
+                di[sender] = di[sender] + 1
+            else: 
+                di[sender] = 1
+
+    #print(di) This is how I check if it counted it correctly
+
+    largest = -1
+    value = None
+    for k,v in di.items():
+        if v > largest:
+            largest = v
+            value = k
+    print (value, largest)
+
         
 
 ## if you want to test locally run > python counter.py
 if __name__ == "__main__":
     countEmail()
+
